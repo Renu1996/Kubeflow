b0975b454a7f0a65d67c2ca013b9c9a6580e07ee
Renu Kadolkar <renu.rock@gmail.com>
2019-01-02 15:12:51 +0530

    Committing prerequisite files

---
+++ Prerequisites/student_analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = pd.read_csv('C:/Users/Renu Kadolkar/Downloads/studentsp/StudentsPerformance.csv')
+
+math_mean = data['math score'].mean()
+read_mean = data['reading score'].mean()
+write_mean = data['writing score'].mean()
+
+print("Math score mean = "+str(math_mean))
+print("Reading score mean = "+str(read_mean))
+print("Writing score mean = "+str(write_mean))
+
+grouped_mean = data.groupby('gender').mean()
+print("Subject means grouped by genders are as follows: ")
+print(grouped_mean)
